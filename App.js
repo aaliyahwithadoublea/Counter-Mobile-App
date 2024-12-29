ba4d4046bcc4c08cd1593004bad9143a43de7ae5
aaliyahwithadoublea <111052111+aaliyahwithadoublea@users.noreply.github.com>
2024-12-29 01:51:27 +0100

    counter mobile app

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  state = {
+    value:0,
+    total_taps:0
+  }
+
+  incrementValue = () =>{
+    this.setState({
+      value:this.state.value + 1,
+      total_taps: this.state.total_taps + 1
+    })
+    console.log("value: " + (this.state.value + 1))
+  }
+
+  decrementValue = () =>{
+    this.setState({
+      value:this.state.value - 1,
+      total_taps: this.state.total_taps + 1
+    })
+    console.log("value: " + (this.state.value - 1))
+  }
+
+
+  render( ){
+    return (
+      <View style={styles.container}>
+        <Text style={{fontSize:60, marginBottom:0}}>{this.state.value}</Text>
+        <Text style={{fontSize:12, marginBottom:20,color:'black'}}>{"Total Taps " 
+          + this.state.total_taps}</Text>
+        <StatusBar styles="auto" />
+        <View style={{flexDirection:'row'}}>
+          <Button onPress={this.decrementValue} title="Decrease" />
+          <Text>   </Text>
+          <Button onPress={this.incrementValue} title="Increase" />
+
+        </View>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container :{
+    flex:1,
+    backgroundColor:"#fff",
+    alignItems:"center",
+    justifyContent:"center"
+  }
+})
